57d68a1cdd9685055e452fa2bd454da749151071
Thomas Biesaart <thomas.biesaart@outlook.com>
2016-06-27 13:37:37 +0200

    Fix integration test with license header

--- xill-runtime/src/test/java/nl/xillio/xill/integration/RobotsIT.java
+++ xill-runtime/src/test/java/nl/xillio/xill/integration/RobotsIT.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +16,7 @@
 package nl.xillio.xill.integration;
 
 
+import me.biesaart.utils.Log;
 import nl.xillio.xill.api.XillEnvironment;
 import nl.xillio.xill.api.XillLoader;
 import nl.xillio.xill.api.XillProcessor;
@@ -23,6 +24,7 @@ import nl.xillio.xill.api.errors.XillParsingException;
 import nl.xillio.xill.integration.embeddedmongo.EmbeddedMongoHelper;
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
+import org.slf4j.Logger;
 import org.testng.annotations.*;
 
 import java.io.IOException;
@@ -34,6 +36,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class RobotsIT {
+    private static final Logger LOGGER = Log.get();
     private Path projectPath = Paths.get("target/integration-test", getClass().getName());
     private Path pluginPath = Paths.get("../xill-ide-launcher/plugins");
     private XillEnvironment xillEnvironment;
@@ -97,6 +100,7 @@ public class RobotsIT {
 
     @Test(dataProvider = "robots")
     public void runRobot(URL robot, String name) throws IOException, XillParsingException {
+        LOGGER.info("Testing {}", name);
         Path robotFile = projectPath.resolve(name);
 
         // cleanup mongo before each test (to make sure previous failures do not influence this test)
