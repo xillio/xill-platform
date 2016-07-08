eebff2c41a38121f2e35466a73b0da65bc66bc88
Luca Scalzotto <lucascalzotto@gmail.com>
2016-07-08 16:14:26 +0200

    Make robot tab icons non-static (fixes bug with the icons)

--- xill-ide-core/src/main/java/nl/xillio/migrationtool/gui/RobotTab.java
+++ xill-ide-core/src/main/java/nl/xillio/migrationtool/gui/RobotTab.java
@@ -66,8 +66,8 @@ public class RobotTab extends FileTab implements Initializable {
     // Status icons.
     private static final String PATH_STATUSICON_RUNNING = "M256,92.481c44.433,0,86.18,17.068,117.553,48.064C404.794,171.411,422,212.413,422,255.999 s-17.206,84.588-48.448,115.455c-31.372,30.994-73.12,48.064-117.552,48.064s-86.179-17.07-117.552-48.064 C107.206,340.587,90,299.585,90,255.999s17.206-84.588,48.448-115.453C169.821,109.55,211.568,92.481,256,92.481 M256,52.481 c-113.771,0-206,91.117-206,203.518c0,112.398,92.229,203.52,206,203.52c113.772,0,206-91.121,206-203.52 C462,143.599,369.772,52.481,256,52.481L256,52.481z M206.544,357.161V159.833l160.919,98.666L206.544,357.161z";
     private static final String PATH_STATUSICON_PAUSED = "M256,92.481c44.433,0,86.18,17.068,117.553,48.064C404.794,171.411,422,212.413,422,255.999 s-17.206,84.588-48.448,115.455c-31.372,30.994-73.12,48.064-117.552,48.064s-86.179-17.07-117.552-48.064 C107.206,340.587,90,299.585,90,255.999s17.206-84.588,48.448-115.453C169.821,109.55,211.568,92.481,256,92.481 M256,52.481 c-113.771,0-206,91.117-206,203.518c0,112.398,92.229,203.52,206,203.52c113.772,0,206-91.121,206-203.52 C462,143.599,369.772,52.481,256,52.481L256,52.481z M240.258,346h-52.428V166h52.428V346z M326.17,346h-52.428V166h52.428V346z";
-    private static final Group STATUSICON_RUNNING = createIcon(PATH_STATUSICON_RUNNING);
-    private static final Group STATUSICON_PAUSED = createIcon(PATH_STATUSICON_PAUSED);
+    private final Group statusIconRunning = createIcon(PATH_STATUSICON_RUNNING);
+    private final Group statusIconPaused = createIcon(PATH_STATUSICON_PAUSED);
 
     @FXML
     private HBox hbxBot;
@@ -135,14 +135,14 @@ public class RobotTab extends FileTab implements Initializable {
                 });
 
         // Status icons
-        STATUSICON_RUNNING.setAutoSizeChildren(true);
-        STATUSICON_PAUSED.setAutoSizeChildren(true);
+        statusIconRunning.setAutoSizeChildren(true);
+        statusIconPaused.setAutoSizeChildren(true);
 
         // Subscribe to start/stop for icon change
-        processor.getDebugger().getOnRobotStart().addListener(e -> Platform.runLater(() -> setGraphic(STATUSICON_RUNNING)));
+        processor.getDebugger().getOnRobotStart().addListener(e -> Platform.runLater(() -> setGraphic(statusIconRunning)));
         processor.getDebugger().getOnRobotStop().addListener(e -> Platform.runLater(() -> setGraphic(null)));
-        processor.getDebugger().getOnRobotPause().addListener(e -> Platform.runLater(() -> setGraphic(STATUSICON_PAUSED)));
-        processor.getDebugger().getOnRobotContinue().addListener(e -> Platform.runLater(() -> setGraphic(STATUSICON_RUNNING)));
+        processor.getDebugger().getOnRobotPause().addListener(e -> Platform.runLater(() -> setGraphic(statusIconPaused)));
+        processor.getDebugger().getOnRobotContinue().addListener(e -> Platform.runLater(() -> setGraphic(statusIconRunning)));
         apnStatusBar.registerDebugger(processor.getDebugger());
     }
 
