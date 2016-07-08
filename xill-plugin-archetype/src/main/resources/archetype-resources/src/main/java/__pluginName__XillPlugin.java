256e4db2e5933da0bbaf22afeae30eaeab9e9414
Thomas Biesaart <thomas.biesaart@outlook.com>
2016-07-08 12:00:14 +0200

    Implement Initial Version of Archetype

---
+++ xill-plugin-archetype/src/main/resources/archetype-resources/src/main/java/__pluginName__XillPlugin.java
@@ -0,0 +1,16 @@
+package ${package};
+
+import nl.xillio.plugins.XillPlugin;
+
+/**
+ * This class represents the base implementation and configuration of your plugin.
+ * Here you can configure any required components for your plugin by overriding any of
+ * the defaults.
+ *
+ * @see <a href="http://guide.xill.io">http://guide.xill.io</a>
+ * @since ${version}
+ * @author ${author}
+ */
+public class ${pluginName}XillPlugin extends XillPlugin {
+
+}
