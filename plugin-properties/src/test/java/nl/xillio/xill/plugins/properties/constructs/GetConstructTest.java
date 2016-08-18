d89033c03e11995dda4099fb5ebb6a461ff19bc1
Thomas Biesaart <thomas.biesaart@gmail.com>
2016-08-18 09:40:22 -0400

    Copy files from repository

---
+++ plugin-properties/src/test/java/nl/xillio/xill/plugins/properties/constructs/GetConstructTest.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) 2014 Xillio (support@xillio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.xillio.xill.plugins.properties.constructs;
+
+import nl.xillio.xill.plugins.properties.services.FileSystemAccess;
+import nl.xillio.xill.plugins.properties.services.PropertyService;
+import nl.xillio.events.EventHost;
+import nl.xillio.xill.TestUtils;
+import nl.xillio.xill.api.NullDebugger;
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.components.RobotID;
+import nl.xillio.xill.api.construct.Construct;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.construct.ConstructProcessor;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.Properties;
+import java.util.UUID;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+
+
+public class GetConstructTest extends TestUtils {
+
+    /**
+     * This test will test normal usage without loading files.
+     */
+    @Test
+    public void testNormalUsageNoFiles() {
+        FileSystemAccess fileSystemAccess = mock(FileSystemAccess.class);
+        when(fileSystemAccess.exists(any())).thenReturn(false);
+
+        GetConstruct construct = new GetConstruct(
+                new PropertyService(new Properties(), fileSystemAccess)
+        );
+
+        MetaExpression notFound = process(construct, fromValue("propertyNotExists"));
+        assertEquals(notFound, NULL);
+
+        MetaExpression notFoundButDefault = process(construct, fromValue("notExists"), fromValue("Hello World"));
+        assertEquals(notFoundButDefault.getStringValue(), "Hello World");
+    }
+
+    @Test
+    public void testNormalUsageNoFilesButDefaults() {
+        FileSystemAccess fileSystemAccess = mock(FileSystemAccess.class);
+        when(fileSystemAccess.exists(any())).thenReturn(false);
+
+        Properties properties = new Properties();
+        properties.put("defaultValue", "YES");
+
+        GetConstruct construct = new GetConstruct(
+                new PropertyService(properties, fileSystemAccess)
+        );
+
+        MetaExpression result = process(construct, fromValue("defaultValue"));
+        assertEquals(result.getStringValue(), "YES");
+    }
+
+    @Override
+    protected MetaExpression process(Construct construct, MetaExpression... arguments) {
+        RobotID robotId = RobotID.getInstance(new File("./libs/test.xill"), new File("."));
+        return ConstructProcessor.process(
+                construct.prepareProcess(
+                        new ConstructContext(
+                                robotId,
+                                robotId,
+                                construct,
+                                new NullDebugger(),
+                                UUID.randomUUID(),
+                                new EventHost<>(),
+                                new EventHost<>()
+                        )
+                ),
+                arguments
+        );
+    }
+}
---
+++ plugin-properties/src/test/java/nl/xillio/xill/plugins/properties/services/PropertyServiceTest.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright (C) 2014 Xillio (support@xillio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.xillio.xill.plugins.properties.services;
+
+import nl.xillio.xill.TestUtils;
+import nl.xillio.xill.api.components.RobotID;
+import nl.xillio.xill.api.construct.ConstructContext;
+import org.apache.commons.io.IOUtils;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+
+
+public class PropertyServiceTest extends TestUtils {
+    @Test
+    public void testGetPropertyFromProjectOverride() throws Exception {
+        Path projectPath = Paths.get("project");
+        RobotID robotID = RobotID.getInstance(projectPath.resolve("robot.xill").toFile(), projectPath.toFile());
+
+        // Mock the file system
+        FileSystemAccess fileSystemAccess = new FileSystemAccess() {
+            @Override
+            public boolean exists(Path file) {
+                return file.equals(projectPath.resolve("xill.properties"));
+            }
+
+            @Override
+            public InputStream read(Path file) throws IOException {
+                return IOUtils.toInputStream("testProperty=Hello World");
+            }
+        };
+
+        PropertyService propertyService = new PropertyService(new Properties(), fileSystemAccess);
+
+        String result = propertyService.getProperty("testProperty", null, new ConstructContext(
+                robotID,
+                robotID,
+                null,
+                null,
+                null,
+                null,
+                null
+        ));
+
+        assertEquals(result, "Hello World");
+    }
+
+    @Test
+    public void testGetPropertyFromRobotOutsideOfProject() {
+        Path projectPath = Paths.get("project");
+        RobotID robotID = RobotID.getInstance(projectPath.resolve("../robot.xill").toFile(), projectPath.toFile());
+
+        // Mock the file system
+        FileSystemAccess fileSystemAccess = new FileSystemAccess() {
+            @Override
+            public boolean exists(Path file) {
+                return file.equals(projectPath.resolve("defaults.properties"));
+            }
+
+            @Override
+            public InputStream read(Path file) throws IOException {
+                return IOUtils.toInputStream("testProperty=Hello World");
+            }
+        };
+
+        PropertyService propertyService = new PropertyService(new Properties(), fileSystemAccess);
+
+
+        String result = propertyService.getProperty("testProperty", null, new ConstructContext(
+                robotID,
+                robotID,
+                null,
+                null,
+                null,
+                null,
+                null
+        ));
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testGetPropertyDoesNotFailOnIOException() {
+        Path projectPath = Paths.get("project");
+        RobotID robotID = RobotID.getInstance(projectPath.resolve("../robot.xill").toFile(), projectPath.toFile());
+
+        // Mock the file system
+        FileSystemAccess fileSystemAccess = new FileSystemAccess() {
+            @Override
+            public boolean exists(Path file) {
+                return true;
+            }
+
+            @Override
+            public InputStream read(Path file) throws IOException {
+                throw new IOException("UNIT TEST");
+            }
+        };
+
+        PropertyService propertyService = new PropertyService(new Properties(), fileSystemAccess);
+
+
+        propertyService.getProperty("testProperty", null, new ConstructContext(
+                robotID,
+                robotID,
+                null,
+                null,
+                null,
+                null,
+                null
+        ));
+    }
+
+    @Test
+    public void testPropertySubstitution() {
+        Properties properties = new Properties();
+        properties.put("name", "World");
+        properties.put("greet", "Hello ${name}!");
+
+        String value = new PropertyService(properties, mock(FileSystemAccess.class)).getFormattedProperty("greet", null, new ConstructContext(
+                RobotID.getInstance(new File("test/value.xill"), new File("test")),
+                RobotID.dummyRobot(),
+                null,
+                null,
+                null,
+                null,
+                null
+        ));
+
+        assertEquals(value, "Hello World!");
+    }
+
+    @Test
+    public void testEscapedPropertySubstitution() {
+        Properties properties = new Properties();
+        properties.put("name", "World");
+        properties.put("greet", "Hello $${name}!");
+
+        String value = new PropertyService(properties, mock(FileSystemAccess.class)).getFormattedProperty("greet", null, new ConstructContext(
+                RobotID.getInstance(new File("test/value.xill"), new File("test")),
+                RobotID.dummyRobot(),
+                null,
+                null,
+                null,
+                null,
+                null
+        ));
+
+        assertEquals(value, "Hello $${name}!");
+    }
+}
