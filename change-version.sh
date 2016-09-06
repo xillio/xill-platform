a2ad69581c5b59523e6f768523c669f4520035e3
Titus Nachbauer <titus.nachbauer@xillio.nl>
2016-09-06 15:15:11 +0200

    Add switch to bash script to skip execution of maven goals

--- change-version.sh
+++ change-version.sh
@@ -1,7 +1,10 @@
 #!/bin/sh            
 sed -i "s/<version>.*?<\/version><!-- Xill Platform Version -->/<version>$1<\/version><!-- Xill Platform Version -->/" distpom.xml
 sed -i "s/<xill.version>.*?<\/xill.version>/<xill.version>$1<\/xill.version>/" xill-parent/pom.xml
-mvn versions:set -DnewVersion=$1
-cd xill-parent/
-mvn versions:set -DnewVersion=$1
-cd ..
+if [ $2 != "-jenkins" ]
+	then
+		mvn versions:set -DnewVersion=$1
+		cd xill-parent/
+		mvn versions:set -DnewVersion=$1
+		cd ..
+fi
