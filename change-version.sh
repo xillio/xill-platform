a86ed8889135533f32b462f2be5022c32e3a64e7
Titus Nachbauer <titus.nachbauer@xillio.nl>
2016-09-06 17:11:50 +0200

    Fix change version bash script

--- change-version.sh
+++ change-version.sh
@@ -15,9 +15,9 @@
 # limitations under the License.
 #
 
-sed -i "s/<version>.*?<\/version><!-- Xill Platform Version -->/<version>$1<\/version><!-- Xill Platform Version -->/" distpom.xml
-sed -i "s/<xill.version>.*?<\/xill.version>/<xill.version>$1<\/xill.version>/" xill-parent/pom.xml
-if [ $2 != "-jenkins" ]
+sed -i "s/<version>.*<\/version><!-- Xill Platform Version -->/<version>$1<\/version><!-- Xill Platform Version -->/" distpom.xml
+sed -i "s/<xill.version>.*<\/xill.version>/<xill.version>$1<\/xill.version>/" xill-parent/pom.xml
+if [ "$2" != "-jenkins" ]
 	then
 		mvn versions:set -DnewVersion=$1
 		cd xill-parent/
