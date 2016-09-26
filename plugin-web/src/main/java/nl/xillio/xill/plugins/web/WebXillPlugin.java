d11a6c106b677fe3df61675c1223655d582719f3
Titus Nachbauer <titus.nachbauer@xillio.nl>
2016-09-26 14:42:48 +0200

    Fix injection of PhantomJSPool

--- plugin-web/src/main/java/nl/xillio/xill/plugins/web/WebXillPlugin.java
+++ plugin-web/src/main/java/nl/xillio/xill/plugins/web/WebXillPlugin.java
@@ -15,7 +15,8 @@
  */
 package nl.xillio.xill.plugins.web;
 
-import com.google.inject.Inject;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import nl.xillio.plugins.XillPlugin;
 import nl.xillio.xill.api.XillThreadFactory;
 import nl.xillio.xill.plugins.web.data.Options;
@@ -25,25 +26,22 @@ import nl.xillio.xill.plugins.web.data.PhantomJSPool;
  * This package includes all Selenium constructs
  */
 public class WebXillPlugin extends XillPlugin {
-
-    XillThreadFactory xillThreadFactory;
     private PhantomJSPool phantomJSPool;
 
-    @Inject
-    public WebXillPlugin(XillThreadFactory xillThreadFactory) {
+    public WebXillPlugin() {
         Options.extractNativeBinary();
-        this.xillThreadFactory = xillThreadFactory;
-    }
-
-    @Override
-    public void configure() {
-        phantomJSPool = new PhantomJSPool(xillThreadFactory);
-        bind(PhantomJSPool.class).toInstance(phantomJSPool);
     }
 
     @Override
     public void close() throws Exception {
-        super.close();
         phantomJSPool.close();
+        super.close();
+    }
+
+    @Singleton
+    @Provides
+    public PhantomJSPool providesPhantomJSPool(XillThreadFactory xillThreadFactory) {
+        phantomJSPool = new PhantomJSPool(xillThreadFactory);
+        return phantomJSPool;
     }
 }
