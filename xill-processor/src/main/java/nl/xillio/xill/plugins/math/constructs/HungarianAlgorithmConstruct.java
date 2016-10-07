6da430b88cda7b32046c93433dace803729ddf55
Luca Scalzotto <lucascalzotto@gmail.com>
2016-10-07 14:02:24 +0200

    Change result to be an object, fix tests

--- xill-processor/src/main/java/nl/xillio/xill/plugins/math/constructs/HungarianAlgorithmConstruct.java
+++ xill-processor/src/main/java/nl/xillio/xill/plugins/math/constructs/HungarianAlgorithmConstruct.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -149,10 +149,8 @@ public class HungarianAlgorithmConstruct extends Construct {
         }
 
         // Prepare results
-        List<MetaExpression> result = new ArrayList<>();
-        LinkedHashMap<String, MetaExpression> sumMap = new LinkedHashMap<>();
-        sumMap.put("sum", fromValue(sum));
-        result.add(fromValue(sumMap));
+        LinkedHashMap<String, MetaExpression> result = new LinkedHashMap<>();
+        result.put("sum", fromValue(sum));
 
         List<MetaExpression> cells = new ArrayList<>();
         for (int[] element : assignment) {
@@ -161,9 +159,7 @@ public class HungarianAlgorithmConstruct extends Construct {
             item.put("col", fromValue(element[1]));
             cells.add(fromValue(item));
         }
-        LinkedHashMap<String, MetaExpression> cellsMap = new LinkedHashMap<>();
-        cellsMap.put("cells", fromValue(cells));
-        result.add(fromValue(cellsMap));
+        result.put("cells", fromValue(cells));
 
         return fromValue(result);
     }
--- xill-processor/src/test/java/nl/xillio/xill/plugins/math/constructs/HungarianAlgorithmConstructTest.java
+++ xill-processor/src/test/java/nl/xillio/xill/plugins/math/constructs/HungarianAlgorithmConstructTest.java
@@ -23,17 +23,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * Test the {@link HungarianAlgorithmConstruct}.
  */
 public class HungarianAlgorithmConstructTest extends TestUtils {
-
     /**
      * <p>
      * Test the process method under normal circumstances.
@@ -45,77 +42,53 @@ public class HungarianAlgorithmConstructTest extends TestUtils {
      */
     @Test
     public void processNormalUsage() {
-        // Mock
-        MetaExpression matrix = mock(MetaExpression.class);
-        String hungarianReturnValue = "[{\"sum\":10.0},{\"cells\":[{\"row\":0,\"col\":2},{\"row\":1,\"col\":1},{\"row\":2,\"col\":0}]}]";
-        when(matrix.getType()).thenReturn(LIST);
-        when(matrix.getValue()).thenReturn(Arrays.asList(
+        String hungarianReturnValue = "{\"sum\":10.0,\"cells\":[{\"row\":0,\"col\":2},{\"row\":1,\"col\":1},{\"row\":2,\"col\":0}]}";
+
+        // Variables.
+        MetaExpression matrix = fromValue(Arrays.asList(
                 fromValue(Arrays.asList(fromValue(0), fromValue(1), fromValue(3))),
                 fromValue(Arrays.asList(fromValue(2), fromValue(2), fromValue(3))),
-                fromValue(Arrays.asList(fromValue(5), fromValue(4), fromValue(1)))));
+                fromValue(Arrays.asList(fromValue(5), fromValue(4), fromValue(1)))
+        ));
+        MetaExpression max = fromValue(true);
 
-        MetaExpression max = mock(MetaExpression.class);
-        when(max.getBooleanValue()).thenReturn(true);
-
-        // Run
+        // Run.
         MetaExpression result = HungarianAlgorithmConstruct.process(matrix, max);
 
-        // Verify
-        // nothing to verify
-
-        // Assert
-        // Check wheter the result is correct as String value.
+        // Assert.
         Assert.assertEquals(result.toString(), hungarianReturnValue);
 
-        // Check if the result is a list, if so, check if its children are objects
-        Assert.assertEquals(result.getType(), LIST);
-        @SuppressWarnings("unchecked")
-        List<MetaExpression> resultAsList = (List<MetaExpression>) result.getValue();
-        Assert.assertEquals(resultAsList.size(), 2);
-        Assert.assertEquals(resultAsList.get(0).getType(), OBJECT);
-        Assert.assertEquals(resultAsList.get(1).getType(), OBJECT);
+        // Check if the result is an object and if the children are the correct type.
+        Assert.assertEquals(result.getType(), OBJECT);
+        LinkedHashMap<String, MetaExpression> resultMap = result.getValue();
+        Assert.assertEquals(resultMap.size(), 2);
+        Assert.assertEquals(resultMap.get("sum").getType(), ATOMIC);
+        Assert.assertEquals(resultMap.get("cells").getType(), LIST);
 
-        // Check wheter the second object contains a list, if so, check if its children are objects
-        @SuppressWarnings("unchecked")
-        LinkedHashMap<String, MetaExpression> secondItem = (LinkedHashMap<String, MetaExpression>) resultAsList.get(1).getValue();
-        Assert.assertEquals(secondItem.get("cells").getType(), LIST);
-        @SuppressWarnings("unchecked")
-        List<MetaExpression> secondItemAsList = (List<MetaExpression>) secondItem.get("cells").getValue();
-        Assert.assertEquals(secondItemAsList.size(), 3);
-        Assert.assertEquals(secondItemAsList.get(0).getType(), OBJECT);
-        Assert.assertEquals(secondItemAsList.get(1).getType(), OBJECT);
-        Assert.assertEquals(secondItemAsList.get(2).getType(), OBJECT);
+        // Check if all cells are objects.
+        List<MetaExpression> cellsList = resultMap.get("cells").getValue();
+        Assert.assertEquals(cellsList.size(), 3);
+        Assert.assertEquals(cellsList.get(0).getType(), OBJECT);
+        Assert.assertEquals(cellsList.get(1).getType(), OBJECT);
+        Assert.assertEquals(cellsList.get(2).getType(), OBJECT);
     }
 
     /**
      * Tests whether the exception is thrown when there are too little rows provided.
      */
-    @Test(expectedExceptions = InvalidUserInputException.class, expectedExceptionsMessageRegExp = "Not enough data..*At least 1 row with data..*")
+    @Test(expectedExceptions = InvalidUserInputException.class, expectedExceptionsMessageRegExp = "Not enough data\\..*At least 1 row with data\\..*")
     public void processTooFewRows() {
-
-        MetaExpression matrix = mock(MetaExpression.class);
-        when(matrix.getType()).thenReturn(LIST);
-        when(matrix.getValue()).thenReturn(Arrays.asList());
-
-        MetaExpression max = mock(MetaExpression.class);
-        when(max.getBooleanValue()).thenReturn(true);
-
-        HungarianAlgorithmConstruct.process(matrix, max);
+        HungarianAlgorithmConstruct.process(emptyList(), fromValue(true));
     }
 
     /**
      * Tests whether the exception is thrown when enough rows but too little columns are provided.
      */
-    @Test(expectedExceptions = InvalidUserInputException.class, expectedExceptionsMessageRegExp = "Not enough data..*At least 1 column with data..*")
+    @Test(expectedExceptions = InvalidUserInputException.class, expectedExceptionsMessageRegExp = "Not enough data\\..*At least 1 column with data\\..*")
     public void processTooFewColumns() {
-        MetaExpression matrix = mock(MetaExpression.class);
-        when(matrix.getType()).thenReturn(LIST);
-        when(matrix.getValue()).thenReturn(Arrays.asList(fromValue(Arrays.asList())));
+        MetaExpression matrix = fromValue(Collections.singletonList(emptyList()));
 
-        MetaExpression max = mock(MetaExpression.class);
-        when(max.getBooleanValue()).thenReturn(true);
-
-        HungarianAlgorithmConstruct.process(matrix, max);
+        HungarianAlgorithmConstruct.process(matrix, fromValue(true));
     }
 
     /**
@@ -123,17 +96,11 @@ public class HungarianAlgorithmConstructTest extends TestUtils {
      */
     @Test(expectedExceptions = RobotRuntimeException.class, expectedExceptionsMessageRegExp = "Invalid value `error` in matrix at \\[1,1\\]")
     public void processInvalidInputInMatrix() {
-        // Mock
-        MetaExpression matrix = mock(MetaExpression.class);
-        when(matrix.getType()).thenReturn(LIST);
-        when(matrix.getValue()).thenReturn(Arrays.asList(
-                fromValue(Arrays.asList(fromValue(0), fromValue(1), fromValue(3))),
-                fromValue(Arrays.asList(fromValue(2), fromValue("error"), fromValue(3))),
-                fromValue(Arrays.asList(fromValue(5), fromValue(4), fromValue(1)))));
+        MetaExpression matrix = fromValue(Arrays.asList(
+                fromValue(Arrays.asList(fromValue(0), fromValue(1))),
+                fromValue(Arrays.asList(fromValue(2), fromValue("error")))
+        ));
 
-        MetaExpression max = mock(MetaExpression.class);
-        when(max.getBooleanValue()).thenReturn(true);
-        // Run
-        HungarianAlgorithmConstruct.process(matrix, max);
+        HungarianAlgorithmConstruct.process(matrix, fromValue(true));
     }
 }
