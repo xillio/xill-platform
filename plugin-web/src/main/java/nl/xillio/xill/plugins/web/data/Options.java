e3f4827a9520f0015d2ddf69a0a6a7440502e185
Thomas Biesaart <thomas.biesaart@gmail.com>
2016-10-11 16:25:35 +0200

    Add distributionManagement

--- plugin-web/src/main/java/nl/xillio/xill/plugins/web/data/Options.java
+++ plugin-web/src/main/java/nl/xillio/xill/plugins/web/data/Options.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2014 Xillio (support@xillio.com)
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -385,7 +385,6 @@ public class Options implements MetadataExpression {
 
             try (InputStream reader = WebXillPlugin.class.getResourceAsStream(phantomJsResourcePath); FileOutputStream writer = new FileOutputStream(PHANTOM_JS_BIN)) {
                 IOUtils.copy(reader, writer);
-
             }
         } catch (Exception e) {
             LOGGER.error("Failed to deploy PhantomJS binary", e);
