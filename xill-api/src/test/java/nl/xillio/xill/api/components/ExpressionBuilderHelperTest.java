1edaa06c7266f01f7d1f1d0833ef8f8c1eec2228
Geert Konijnendijk <geert@konijnendijk.info>
2016-10-13 15:27:09 +0200

    Add tests for literals in ExpressionBuilderHelper

---
+++ xill-api/src/test/java/nl/xillio/xill/api/components/ExpressionBuilderHelperTest.java
@@ -0,0 +1,60 @@
+package nl.xillio.xill.api.components;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Tests for the methods and fields of {@link ExpressionBuilderHelper}
+ *
+ * @author Geert Konijnendijk
+ */
+public class ExpressionBuilderHelperTest {
+
+    private ExpressionBuilderHelper expressionBuilderHelper;
+
+    /**
+     * Create a new instance of {@link ExpressionBuilderHelper} before each test
+     */
+    @BeforeMethod
+    public void createExpressionBuilderHelper() {
+        expressionBuilderHelper = new ExpressionBuilderHelper();
+    }
+
+    /**
+     * Test {@link ExpressionBuilderHelper#TRUE}
+     */
+    @Test
+    public void testTrueExpression() {
+        MetaExpression trueExpression = ExpressionBuilderHelper.TRUE;
+        assertTrue(trueExpression.getBooleanValue());
+        assertEquals(trueExpression.getStringValue(), "true");
+        assertEquals(trueExpression.getNumberValue(), 1);
+    }
+
+    /**
+     * Test {@link ExpressionBuilderHelper#FALSE}
+     */
+    @Test
+    public void testFalseExpression() {
+        MetaExpression trueExpression = ExpressionBuilderHelper.FALSE;
+        assertFalse(trueExpression.getBooleanValue());
+        assertEquals(trueExpression.getStringValue(), "false");
+        assertEquals(trueExpression.getNumberValue(), 0);
+    }
+
+    /**
+     * Test {@link ExpressionBuilderHelper#NULL}
+     */
+    @Test
+    public void testNullExpression() {
+        MetaExpression trueExpression = ExpressionBuilderHelper.NULL;
+        assertFalse(trueExpression.getBooleanValue());
+        assertEquals(trueExpression.getStringValue(), "null");
+        assertEquals(trueExpression.getNumberValue(), Double.NaN);
+    }
+
+}
