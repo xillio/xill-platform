e43817f9647592c84e8a12ade44b366b847e0bb3
Geert Konijnendijk <geert@konijnendijk.info>
2016-10-26 09:34:21 +0200

    Add unit tests for copying metaexpressions

--- xill-api/src/test/java/nl/xillio/xill/api/components/MetaExpressionTest.java
+++ xill-api/src/test/java/nl/xillio/xill/api/components/MetaExpressionTest.java
@@ -19,14 +19,19 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import nl.xillio.xill.api.data.DateFactory;
+import nl.xillio.xill.api.data.MetadataExpression;
+import nl.xillio.xill.api.io.IOStream;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Field;
 import java.sql.Date;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Collection;
 
+import static org.mockito.Mockito.*;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
 
 
 public class MetaExpressionTest {
@@ -79,5 +84,103 @@ public class MetaExpressionTest {
                 );
             }
         });
+    }
+
+    /**
+     * Test {@link MetaExpression#copy()}
+     */
+    @Test
+    public void testCopyWithMetadata() {
+        // Mock
+        MetaExpression copy = mock(MetaExpression.class);
+        MetaExpression test = new TestingMetadataExpression(copy);
+        MetadataExpression notCopyable = mock(MetadataExpression.class);
+        test.storeMeta(notCopyable);
+        CopyableMetadataExpression copyable = mock(CopyableMetadataExpression.class);
+        CopyableMetadataExpression copied =mock(CopyableMetadataExpression.class);
+        when(copyable.copy()).thenReturn(copied);
+        test.storeMeta(copyable);
+
+        // Run
+        MetaExpression result = test.copy();
+
+        // Verify
+        verify(copy).storeMeta(copied);
+        verifyNoMoreInteractions(copied, notCopyable);
+
+        // Assert
+        assertSame(result, copy);
+    }
+
+    /**
+     * Test {@link MetaExpression#copy()}
+     */
+    @Test
+    public void testCopy() {
+        // Mock
+        MetaExpression copy = mock(MetaExpression.class);
+        MetaExpression test = new TestingMetadataExpression(copy);
+        MetadataExpression notCopyable = mock(MetadataExpression.class);
+        test.storeMeta(notCopyable);
+
+        // Run
+        MetaExpression result = test.copy();
+
+        // Verify
+        verifyNoMoreInteractions(notCopyable);
+
+        // Assert
+        assertSame(result, copy);
+    }
+
+    /**
+     * Stub implementation of MetaExpression for testing
+     */
+    private class TestingMetadataExpression extends MetaExpression {
+
+        private MetaExpression copy;
+
+        /**
+         *
+         * @param copy Returned when {@link MetaExpression#copyExpression()} is called
+         */
+        public TestingMetadataExpression(MetaExpression copy) {
+            this.copy = copy;
+        }
+
+        @Override
+        public Number getNumberValue() {
+            return null;
+        }
+
+        @Override
+        public String getStringValue() {
+            return null;
+        }
+
+        @Override
+        public boolean getBooleanValue() {
+            return false;
+        }
+
+        @Override
+        public boolean isNull() {
+            return false;
+        }
+
+        @Override
+        public IOStream getBinaryValue() {
+            return null;
+        }
+
+        @Override
+        public Collection<Processable> getChildren() {
+            return null;
+        }
+
+        @Override
+        protected MetaExpression copyExpression() {
+            return copy;
+        }
     }
 }
