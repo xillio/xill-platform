436166393b2bb80ae6dedfcf586dceac5c2f04d3
Pieter Dirk Soels <pieter.soels@xillio.nl>
2016-11-03 17:45:54 +0100

    Add unit tests for the RangeIteratorFactory.class

--- xill-processor/src/test/java/nl/xillio/xill/plugins/collection/data/range/RangeIteratorFactoryTest.java
+++ xill-processor/src/test/java/nl/xillio/xill/plugins/collection/data/range/RangeIteratorFactoryTest.java
@@ -15,11 +15,122 @@
  */
 package nl.xillio.xill.plugins.collection.data.range;
 
+import nl.xillio.xill.TestUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * The test class for {@link RangeIteratorFactory}.
  *
  * @author Pieter Soels
  */
-public class RangeIteratorFactoryTest {
+public class RangeIteratorFactoryTest extends TestUtils {
+    @Test
+    public void testNormalUsageWithoutStepAscending() throws Exception {
+        // Initialize
+        RangeIteratorFactory rangeIteratorFactory = new RangeIteratorFactory();
 
+        // Run
+        RangeIterator iterator = rangeIteratorFactory.createIterator(0, 10, null);
+
+        // Verify
+        ArrayList<Number> output = new ArrayList<>();
+        Number[] expectedOutputArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        ArrayList<Number> expectedOutput = new ArrayList<>(Arrays.asList(expectedOutputArray));
+
+        Assert.assertTrue(iterator.hasNext());
+        while (iterator.hasNext()) {
+            output.add(iterator.next());
+        }
+        Assert.assertFalse(iterator.hasNext());
+
+        Assert.assertEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void testNormalUsageWithoutStepDescending() throws Exception {
+        // Initialize
+        RangeIteratorFactory rangeIteratorFactory = new RangeIteratorFactory();
+
+        // Run
+        RangeIterator iterator = rangeIteratorFactory.createIterator(10, 0, null);
+
+        // Verify
+        ArrayList<Number> output = new ArrayList<>();
+        Number[] expectedOutputArray = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        ArrayList<Number> expectedOutput = new ArrayList<>(Arrays.asList(expectedOutputArray));
+
+        Assert.assertTrue(iterator.hasNext());
+        while (iterator.hasNext()) {
+            output.add(iterator.next());
+        }
+        Assert.assertFalse(iterator.hasNext());
+
+        Assert.assertEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void testNormalUsageWithStepAscending() throws Exception {
+        // Initialize
+        RangeIteratorFactory rangeIteratorFactory = new RangeIteratorFactory();
+
+        // Run
+        RangeIterator iterator = rangeIteratorFactory.createIterator(0, 10, 2);
+
+        // Verify
+        ArrayList<Number> output = new ArrayList<>();
+        Number[] expectedOutputArray = {0, 2, 4, 6, 8};
+        ArrayList<Number> expectedOutput = new ArrayList<>(Arrays.asList(expectedOutputArray));
+
+        Assert.assertTrue(iterator.hasNext());
+        while (iterator.hasNext()) {
+            output.add(iterator.next());
+        }
+        Assert.assertFalse(iterator.hasNext());
+
+        Assert.assertEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void testNormalUsageWithStepDescending() throws Exception {
+        // Initialize
+        RangeIteratorFactory rangeIteratorFactory = new RangeIteratorFactory();
+
+        // Run
+        RangeIterator iterator = rangeIteratorFactory.createIterator(10, -10, -1.5);
+
+        // Verify
+        ArrayList<Number> output = new ArrayList<>();
+        Number[] expectedOutputArray = {10, 8.5, 7.0, 5.5, 4.0, 2.5, 1.0, -0.5, -2.0, -3.5, -5.0, -6.5, -8.0, -9.5};
+        ArrayList<Number> expectedOutput = new ArrayList<>(Arrays.asList(expectedOutputArray));
+
+        Assert.assertTrue(iterator.hasNext());
+        while (iterator.hasNext()) {
+            output.add(iterator.next());
+        }
+        Assert.assertFalse(iterator.hasNext());
+
+        Assert.assertEquals(output, expectedOutput);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testZeroStepException() throws Exception {
+        // Initialize
+        RangeIteratorFactory rangeIteratorFactory = new RangeIteratorFactory();
+
+        // Run
+        RangeIterator iterator = rangeIteratorFactory.createIterator(10, 0, 0);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testDoubleMinValueStepException() throws Exception {
+        // Initialize
+        RangeIteratorFactory rangeIteratorFactory = new RangeIteratorFactory();
+
+        // Run
+        rangeIteratorFactory.createIterator(10, 0, Double.MIN_VALUE);
+    }
 }
