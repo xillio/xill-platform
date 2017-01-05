a8c157a7c027991b17a3928cee2fd6881429ff4d
Xavier <xavier.pardonnet@xillio.nl>
2017-01-05 13:16:43 +0100

    Add test for PoolManagerServiceImpl

---
+++ xill-web-service/src/test/java/nl/xillio/xill/webservice/services/PoolManagerServiceImplTest.java
@@ -0,0 +1,18 @@
+package nl.xillio.xill.webservice.services;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * This class represents
+ *
+ * @author Xillio
+ */
+public class PoolManagerServiceImplTest {
+
+    @Test
+    public void testGetDefaultWorkerPool() throws Exception {
+
+    }
+}
