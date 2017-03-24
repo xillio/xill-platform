8a2d490502726521cf6d5b28bcf14cdb1807d887
Pieter Dirk Soels <p.d.soels@hotmail.com>
2017-03-24 12:18:11 +0100

    Update license headers and remove unused import

--- xill-processor/src/main/java/nl/xillio/xill/plugins/date/constructs/IsDateConstruct.java
+++ xill-processor/src/main/java/nl/xillio/xill/plugins/date/constructs/IsDateConstruct.java
@@ -22,7 +22,6 @@ import nl.xillio.xill.api.construct.ConstructContext;
 import nl.xillio.xill.api.construct.ConstructProcessor;
 import nl.xillio.xill.api.data.Date;
 import nl.xillio.xill.plugins.date.BaseDateConstruct;
-import nl.xillio.xill.plugins.date.services.DateService;
 
 /**
  * Determines if the given object is a valid date.
