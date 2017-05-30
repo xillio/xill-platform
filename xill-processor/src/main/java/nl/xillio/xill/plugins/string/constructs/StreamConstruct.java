1723ebf44b1692131e3b369613d41a4015e781f1
Luca Scalzotto <lucascalzotto@gmail.com>
2017-05-30 11:49:34 +0000

    Merged in CF-21-handle-all-binary-content-as-strea (pull request #268)
    CF-21 Add String.stream construct
    
    * Add String.stream construct and unit test
    
    * Add integration test and documentation
    
    * Add charset option to String.stream construct
    
    * Add test case for special characters
    
    * Add tests with diacritics and specials chars
    
    * Add charset to documentation
    
    * Update changelog
    
    * Suppress sonar warning
    
    Approved-by: Thomas Biesaart <thomas.biesaart@outlook.com>
    Approved-by: Mark de Wit <mark.dewit@xillio.nl>

---
+++ xill-processor/src/main/java/nl/xillio/xill/plugins/string/constructs/StreamConstruct.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2014 Xillio (support@xillio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.xillio.xill.plugins.string.constructs;
+
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.construct.Argument;
+import nl.xillio.xill.api.construct.Construct;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.construct.ConstructProcessor;
+import nl.xillio.xill.api.errors.InvalidUserInputException;
+import nl.xillio.xill.api.io.SimpleIOStream;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+
+public class StreamConstruct extends Construct {
+    @Override
+    public ConstructProcessor prepareProcess(ConstructContext context) {
+        return new ConstructProcessor(this::process, new Argument("value", ATOMIC), new Argument("charset", fromValue("UTF-8")));
+    }
+
+    // The InputStream is closed by Xill resource management
+    @SuppressWarnings("squid:S2095")
+    private MetaExpression process(MetaExpression value, MetaExpression charset) {
+        String text = value.isNull() ? "" : value.getStringValue();
+
+        try {
+            return fromValue(new SimpleIOStream(
+                    IOUtils.toInputStream(text, charset.getStringValue()),
+                    "Streamed Text"
+            ));
+        } catch (IOException | IllegalArgumentException e) {
+            throw new InvalidUserInputException(
+                    "Unknown Character Set",
+                    charset.getStringValue(),
+                    "A valid character set such as 'UTF-8' or 'ISO-8859-1'",
+                    "String.Stream(\"Hello World\", \"UTF-8\");"
+            );
+        }
+    }
+}
