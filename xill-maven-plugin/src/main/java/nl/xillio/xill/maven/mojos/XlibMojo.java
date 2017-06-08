1c6acec78f8049e5d01fa9c1d0e53e25c1bb3dbe
dwight peters <dwight.peters@xillio.nl>
2017-06-08 12:00:54 +0000

    Merged in CF-31-add-option-for-fat-archives-built- (pull request #271)
    CF-31 add option for fat archives built
    
    Approved-by: Thomas Biesaart <thomas.biesaart@outlook.com>
    Approved-by: Mark de Wit <mark.dewit@xillio.nl>

--- xill-maven-plugin/src/main/java/nl/xillio/xill/maven/mojos/XlibMojo.java
+++ xill-maven-plugin/src/main/java/nl/xillio/xill/maven/mojos/XlibMojo.java
@@ -25,19 +25,32 @@ import org.codehaus.plexus.archiver.Archiver;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Mojo(name = "xlib", defaultPhase = LifecyclePhase.PACKAGE, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class XlibMojo extends AbstractXlibMojo {
     @Parameter(defaultValue = "${project.artifact}", readonly = true, required = true)
     private Artifact artifact;
-    @Parameter(defaultValue = "${project.build.finalName}", readonly = true, required = true)
+    @Parameter(defaultValue = "${project.build.finalName}", required = true)
     private String finalName;
+    @Parameter(defaultValue = "${project.artifacts}", readonly = true, required = true)
+    private Collection<Artifact> artifacts;
 
     /**
      * The output directory to create the xlib archive in.
      */
     @Parameter(defaultValue = "${project.build.directory}", required = true)
     private File outputDirectory;
+
+    /**
+     * Whether to create a fat archive containing all dependencies from this project.
+     */
+    @Parameter(defaultValue = "false", required = true, property = "includeDependencies")
+    private boolean includeDependencies;
 
     @Component(role = Archiver.class, hint = "zip")
     private Archiver archiver;
@@ -60,15 +73,17 @@ public class XlibMojo extends AbstractXlibMojo {
     }
 
     public void execute() throws MojoExecutionException {
-        File archive = createArchive();
-
         // Check if the project already has an artifact.
         File artifactFile = artifact.getFile();
         if (artifactFile != null && artifactFile.isFile()) {
             throw new MojoExecutionException("An artifact is already set for this project.");
         }
 
-        artifact.setFile(archive);
+        if (includeDependencies) {
+            collectFatArchiveRobots();
+        }
+
+        artifact.setFile(createArchive());
     }
 
     private File createArchive() throws MojoExecutionException {
@@ -84,6 +99,78 @@ public class XlibMojo extends AbstractXlibMojo {
             return archive;
         } catch (IOException e) {
             throw new MojoExecutionException("Error assembling xlib.", e);
+        }
+    }
+
+    private void collectFatArchiveRobots() throws MojoExecutionException {
+        List<Artifact> artifactList = new ArrayList<>(artifacts);
+
+        // Get all resources from this project, these should not be overwritten.
+        Set<String> resourceSet = getFilesFromProject();
+
+        // Reverse for-loop to retain the correct overriding robot precedence.
+        for (int i = artifactList.size() - 1; i >= 0; i--) {
+            Artifact artifact = artifactList.get(i);
+
+            // Skip non-Xill dependencies.
+            if (!"xlib".equals(artifact.getType())) {
+                getLog().warn("Skipping non-xlib artifact: " + artifact.getId());
+                continue;
+            }
+
+            // Get the file system for the xlib.
+            Path artifactPath = artifact.getFile().toPath();
+            extractArchive(artifactPath, resourceSet);
+        }
+    }
+
+    private void extractArchive(Path archive, Set<String> resourceSet) throws MojoExecutionException {
+        URI artifactUri = URI.create("jar:" + archive.toUri());
+
+        // Get the file system for the archive.
+        FileSystem fs;
+        try {
+            Map<String, String> env = new HashMap<>();
+            env.put("create", "true");
+            fs = FileSystems.newFileSystem(artifactUri, env);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Could not read from xlib: " + archive, e);
+        }
+
+        // Copy all files.
+        Path classesDir = getClassesDirectory();
+        try {
+            Files.walkFileTree(fs.getPath(FileSetFactory.ROBOTS_DIRECTORY), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    // Get the file name without the "robots/" prefix, copy the file.
+                    String fileName = file.toString().substring(FileSetFactory.ROBOTS_DIRECTORY.length() + 1);
+
+                    // If the file already existed in this project, don't overwrite it.
+                    if (!resourceSet.contains(fileName)) {
+                        Path target = classesDir.resolve(fileName);
+                        Files.createDirectories(target.getParent());
+                        Files.copy(file, target, StandardCopyOption.REPLACE_EXISTING);
+                    }
+
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            throw new MojoExecutionException("Could not copy file from artifact: " + archive, e);
+        }
+    }
+
+    private Set<String> getFilesFromProject() throws MojoExecutionException {
+        int start = getClassesDirectory().toString().length() + 1;
+        try {
+            return Files.walk(getClassesDirectory())
+                    .filter(Files::isRegularFile)
+                    .map(Path::toString)
+                    .map(s -> s.substring(start))
+                    .collect(Collectors.toSet());
+        } catch (IOException e) {
+            throw new MojoExecutionException("Could not get list of files.", e);
         }
     }
 }
