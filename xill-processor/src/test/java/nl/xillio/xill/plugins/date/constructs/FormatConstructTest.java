0ff0555b884a6962d8a2db1d3f105bd92c1dd879
Thomas Biesaart <thomas.biesaart@gmail.com>
2017-07-04 17:22:08 +0200

    Create test for FormatConstruct

--- xill-processor/src/test/java/nl/xillio/xill/plugins/date/constructs/FormatConstructTest.java
+++ xill-processor/src/test/java/nl/xillio/xill/plugins/date/constructs/FormatConstructTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2014 Xillio (support@xillio.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,54 +16,81 @@
 package nl.xillio.xill.plugins.date.constructs;
 
 import nl.xillio.xill.TestUtils;
-import nl.xillio.xill.api.components.MetaExpression;
-import nl.xillio.xill.plugins.date.services.DateService;
-import org.testng.annotations.DataProvider;
+import nl.xillio.xill.api.errors.InvalidUserInputException;
+import nl.xillio.xill.api.errors.OperationFailedException;
+import nl.xillio.xill.plugins.date.services.DateServiceImpl;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import static nl.xillio.xill.plugins.date.constructs.IsBeforeConstructTest.createDateTimeExpression;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
 import static org.testng.Assert.assertEquals;
 
 /**
  * Test the {@link FormatConstruct}
  *
- * @author Geert Konijnendijk
  */
 public class FormatConstructTest extends TestUtils {
-    @DataProvider(name = "format")
-    private Object[][] formatProvider() {
-        ZonedDateTime date = ZonedDateTime.now();
-        return new Object[][]{
-                {createDateTimeExpression(date), fromValue("yyyy-MM-dd"), NULL},
-                {createDateTimeExpression(date), fromValue("yyyy-MM-dd"), fromValue("en-US")},
-                {createDateTimeExpression(date), NULL, NULL},
-                {createDateTimeExpression(date), NULL, fromValue("en-US")}};
+    private final FormatConstruct construct = new FormatConstruct();
+
+    @BeforeClass
+    public void initializeConstruct() {
+        construct.setDateService(new DateServiceImpl());
     }
 
-    /**
-     * Test the process method with both null and non-null format variable
-     */
-    @Test(dataProvider = "format")
-    public void testProcess(MetaExpression dateExpression, MetaExpression formatExpression, MetaExpression localeExpression) {
-        // Mock
-        DateService dateService = mock(DateService.class);
-        String returnString = "2015-8-3";
-        when(dateService.formatDate(any(), any(), any())).thenReturn(returnString);
+    @Test
+    public void testDateFormat() {
+        assertEquals(
+                process(
+                        construct,
+                        createDateTimeExpression(
+                                ZonedDateTime.of(1532, 3, 4, 5, 3, 0, 0, ZoneId.of("Europe/Paris"))
+                        ),
+                        fromValue("yyyy-MM-dd")
+                ),
+                fromValue("1532-03-04")
+        );
+    }
 
-        FormatConstruct formatConstruct = new FormatConstruct();
-        formatConstruct.setDateService(dateService);
+    @Test
+    public void testDateFormatWithLocale() {
+        assertEquals(
+                process(
+                        construct,
+                        createDateTimeExpression(
+                                ZonedDateTime.of(1532, 3, 4, 5, 3, 0, 0, ZoneId.of("Europe/Paris"))
+                        ),
+                        fromValue("EEEE"),
+                        fromValue("fr-FR")
+                ),
+                fromValue("vendredi")
+        );
+    }
 
-        // Run
-        MetaExpression formatted = process(formatConstruct, dateExpression, formatExpression, localeExpression);
+    @Test
+    public void testDefaultDateFormat() {
 
-        // Verify
-        verify(dateService).formatDate(any(), any(), any());
+        assertEquals(
+                process(
+                        construct,
+                        createDateTimeExpression(
+                                ZonedDateTime.of(2020, 12, 12, 5, 5, 0, 0, ZoneId.of("UTC"))
+                        )
+                ),
+                fromValue("2020-12-12 05:05:00")
+        );
+    }
 
-        // Assert
-        assertEquals(formatted.getStringValue(), returnString);
+    @Test(expectedExceptions = OperationFailedException.class)
+    public void testInvalidPattern() {
+        process(
+                construct,
+                createDateTimeExpression(
+                        ZonedDateTime.now()
+                ),
+                fromValue("What Am I Doing?")
+        );
     }
 }
