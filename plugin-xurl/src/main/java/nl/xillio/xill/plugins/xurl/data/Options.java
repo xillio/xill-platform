c9697a01648e870ab15d884274babc6568d47bb9
Geert Konijnendijk <geert@konijnendijk.info>
2017-07-20 09:23:14 +0200

    Set default request timeout to 5 minutes
    Warning: this breaks backwards compatibility!
    Set a default timeout for a XURL request. This prevents Xill from hanging when making a request to a web server that does not respond.

--- plugin-xurl/src/main/java/nl/xillio/xill/plugins/xurl/data/Options.java
+++ plugin-xurl/src/main/java/nl/xillio/xill/plugins/xurl/data/Options.java
@@ -30,13 +30,16 @@ import static nl.xillio.xill.plugins.xurl.services.OptionsFactory.parseContentTy
  * @author Thomas Biesaart
  */
 public class Options {
+    // Five minutes
+    private static final int DEFAULT_TIMEOUT = 300000;
+
     private Credentials basicAuth;
     private ProxyOptions proxyOptions;
     private NTLMOptions ntlmOptions;
     private boolean insecure;
     private boolean multipart = false;
     private ContentType responseContentType;
-    private int timeout;
+    private int timeout = DEFAULT_TIMEOUT;
     private Header[] headers = new Header[0];
     private String logging;
     private boolean enableRedirect = true;
