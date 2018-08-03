9516b30b6480603813a8e6effe7c492b9dc052fb
Xavier <xavier.pardonnet@xillio.nl>
2018-08-03 17:55:07 +0200

    Add more tests

--- plugin-document/src/test/java/nl/xillio/xill/plugins/mongodb/constructs/MongoRegexConstructTest.java
+++ plugin-document/src/test/java/nl/xillio/xill/plugins/mongodb/constructs/MongoRegexConstructTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2014 Xillio (support@xillio.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,8 @@ import nl.xillio.xill.api.components.MetaExpression;
 import nl.xillio.xill.plugins.mongodb.data.MongoRegex;
 import org.testng.annotations.Test;
 
+import java.util.regex.PatternSyntaxException;
+
 import static org.testng.Assert.assertEquals;
 
 public class MongoRegexConstructTest extends TestUtils {
@@ -28,5 +30,10 @@ public class MongoRegexConstructTest extends TestUtils {
         MetaExpression result = process(new RegexConstruct(), fromValue(".*"));
         assertEquals(result.getStringValue(), ".*");
         assertEquals(result.getMeta(MongoRegex.class).getPattern().toString(), ".*");
+    }
+
+    @Test(expectedExceptions = PatternSyntaxException.class)
+    public void testConstructWrongRegex() {
+        process(new RegexConstruct(), fromValue("("));
     }
 }
