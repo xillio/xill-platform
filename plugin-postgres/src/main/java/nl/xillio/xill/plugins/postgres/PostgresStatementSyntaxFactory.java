90656b8ae1e8d06d7f2420c56d5bc78db24fcb84
andrea.parrilli <andrea.parrilli@xillio.com>
2019-09-27 16:15:04 +0200

    Implement Postgres as jdbc plugin

---
+++ plugin-postgres/src/main/java/nl/xillio/xill/plugins/postgres/PostgresStatementSyntaxFactory.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2014 Xillio (support@xillio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.xillio.xill.plugins.postgres;
+
+import nl.xillio.xill.plugins.jdbc.services.StatementSyntaxFactoryImpl;
+
+import java.util.Arrays;
+
+public class PostgresStatementSyntaxFactory extends StatementSyntaxFactoryImpl {
+    @Override
+    protected String selectOne(String escapedTableName, String constraints) {
+        return String.format("SELECT * FROM %s WHERE %s LIMIT 1", escapedTableName, constraints);
+    }
+
+    @Override
+    protected String selectOne(String escapedTableName) {
+        return String.format("SELECT * FROM %s LIMIT 1", escapedTableName);
+    }
+
+    @Override
+    protected String escapeTableName(String tableName) {
+        return Arrays.stream(tableName.split("\\."))
+                .map(this::escapeIdentifier)
+                .reduce((a, b) -> a + "." + b)
+                .get();
+    }
+}
